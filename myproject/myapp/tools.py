3a7f6fd74242ccddffac83c2802bec29ce753e58
wisjke <zaizzheniy@gmail.com>
2024-07-30 13:08:50 +0300

    Added city separation

--- myproject/myapp/tools.py
+++ myproject/myapp/tools.py
@@ -1,21 +1,19 @@
 import csv
 import chardet
 import pandas as pd
-
+import re
 
 class ExcelToCsvConverter:
     def __init__(self, excel_file):
-        self.excel_file = excel_file
-        self.csv_file = "Merch.csv"
+        self.excel_file = "Merch.csv"
+        self.excel_file_path = excel_file
 
     def convert(self):
-        df = pd.read_excel(self.excel_file)
-        df.to_csv(self.csv_file, index=False)
+        df = pd.read_excel(self.excel_file_path)
+        df.to_csv(self.excel_file, index=False)
+        return self.excel_file
 
-        return self.csv_file
-
-
-class SaleReader:
+class SalersReader:
     def __init__(self, sale_filename):
         self.sale_filename = sale_filename
 
@@ -23,22 +21,20 @@ class SaleReader:
         encoding = self.detect_encoding(self.sale_filename)
         with open(self.sale_filename, 'r', encoding=encoding) as file:
             reader = csv.DictReader(file, delimiter=';')
-            salers_dk = {}
+            salers = {}
             for line in reader:
                 contact = line["ContactID"]
                 task = line["taskDescription"]
-                # Витягуємо завдання з taskDescription
                 task_number = self.extract_task_number(task)
                 if task_number:
                     task = f"Завдання {task_number}"
-                    if contact not in salers_dk:
-                        salers_dk[contact] = [task]
+                    if contact not in salers:
+                        salers[contact] = [task]
                     else:
-                        salers_dk[contact].append(task)
-        return salers_dk
+                        salers[contact].append(task)
+        return salers
 
     def extract_task_number(self, task_description):
-        import re
         match = re.search(r'Завдання (\d+)', task_description)
         if match:
             return match.group(1)
@@ -50,7 +46,6 @@ class SaleReader:
         result = chardet.detect(raw_data)
         return result['encoding']
 
-
 class MerchReader:
     def __init__(self, merch_filename):
         converter = ExcelToCsvConverter(merch_filename)
@@ -61,19 +56,19 @@ class MerchReader:
         encoding = self.detect_encoding(self.merch_filename)
         with open(self.merch_filename, 'r', encoding=encoding) as file:
             reader = csv.DictReader(file, delimiter=',')
-            dk = {}
+            merchs = {}
             for line in reader:
                 err = line["ERR"]
                 tel = line["tel"]
-                if err in dk:
-                    dk[err].append(tel)
+                city = line["city"]
+                if err not in merchs:
+                    merchs[err] = {'phones': [tel], 'city': city}
                 else:
-                    dk[err] = [tel]
-        return dk
+                    merchs[err]['phones'].append(tel)
+        return merchs
 
     def detect_encoding(self, filename):
         with open(filename, 'rb') as file:
             raw_data = file.read(10000)
         result = chardet.detect(raw_data)
         return result['encoding']
-
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, redirect
 from .forms import UploadForm
 from .models import Upload
-from .tools import SaleReader, MerchReader
+from .tools import SalersReader, MerchReader
 from collections import Counter
-
 
 def slicer(elem):
     return int(elem.split(' ')[1])
-
 
 def upload_files(request):
     if request.method == 'POST':
@@ -21,40 +19,45 @@ def upload_files(request):
         form = UploadForm()
     return render(request, 'upload.html', {'form': form})
 
-
 def process_files(request, pk):
     upload = Upload.objects.get(pk=pk)
     salers_path = upload.salers_file.path
     merch_path = upload.merch_file.path
 
-    salers_dk = {}
-    err_dk = {}
+    salers = {}
+    merchs = {}
     error_message = ""
 
     try:
-        saleReader = SaleReader(salers_path)
-        salers_dk = saleReader.read()
+        salersReader = SalersReader(salers_path)
+        salers = salersReader.read()
     except Exception as e:
         error_message += f"Error reading 'salers.csv': {e}\n"
 
     try:
         merchReader = MerchReader(merch_path)
-        err_dk = merchReader.read()
+        merchs = merchReader.read()
     except Exception as e:
         error_message += f"Error reading 'Merch.xlsx': {e}\n"
 
-    results = []
-    for err in err_dk:
+    city_results = {}
+    for merch, data in merchs.items():
+        city = data['city']
         tasks_list = []
-        for num in err_dk[err]:
-            if num in salers_dk:
-                tasks_list.extend(map(slicer, salers_dk[num]))
+        for num in data['phones']:
+            if num in salers:
+                tasks_list.extend(map(slicer, salers[num]))
         tasks_list.sort()
         counts = Counter(tasks_list)
-        results.append({'error': err, 'counts': dict(counts)})
+        result = {'error': merch, 'counts': dict(counts)}
+
+        if city not in city_results:
+            city_results[city] = [result]
+        else:
+            city_results[city].append(result)
 
     upload.salers_file.delete()
     upload.merch_file.delete()
     upload.delete()
 
-    return render(request, 'results.html', {'results': results, 'error_message': error_message})
+    return render(request, 'results.html', {'city_results': city_results, 'error_message': error_message})
